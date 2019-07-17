63032f9790384e80f50116fd8c080c6f85e02ab6
1098818147 <shawn_shifan@163.com>
2019-07-17 09:23:39 +0800

    Properties and PO files TO CSV

--- src/main/java/com/shawn/poandprotosv/demo/utils/CsvUtils.java
+++ src/main/java/com/shawn/poandprotosv/demo/utils/CsvUtils.java
@@ -1,14 +1,4 @@
-/*
- * ********************************************************************************
- * COPYRIGHT
- *               PAX TECHNOLOGY, Inc. PROPRIETARY INFORMATION
- *   This software is supplied under the terms of a license agreement or
- *   nondisclosure agreement with PAX  Technology, Inc. and may not be copied
- *   or disclosed except in accordance with the terms in that agreement.
- *
- *      Copyright (C) 2017 PAX Technology, Inc. All rights reserved.
- * ********************************************************************************
- */
+
 package com.shawn.poandprotosv.demo.utils;
 
 import java.io.*;
